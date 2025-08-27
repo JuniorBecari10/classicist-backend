9e37f312b1ea850a72144fcea94b9ea649adcd33
Antônio Carlos <antonioocarlos@proton.me>
2025-08-27 13:40:23 -0300

    feat (backend): add data models

---
+++ backend/model/recording.go
@@ -0,0 +1,26 @@
+package model
+
+type Performer struct {
+	Name string
+	Role string // e.g. Conductor, Pianist, Violinist, Orchestra
+}
+
+type RecordedMovement struct {
+	MovementId int
+	RecordingId int
+	File AudioFile
+}
+
+type Recording struct {
+	Id int
+	WorkId int // what is the work this recording is from.
+
+	Performers []Performer
+	FileYear int
+}
+
+type AudioFile struct {
+	Id int
+	Path string
+	Duration int // in seconds
+}
---
+++ backend/model/work.go
@@ -0,0 +1,81 @@
+package model
+
+type Note int
+const (
+	C Note = iota
+	CSharp
+	DFlat
+    DSharp
+	EFlat
+	E
+	F
+	FSharp
+	GFlat
+	G
+	GSharp
+	AFlat
+	A
+	ASharp
+	BFlat
+	B
+)
+
+// no more modes for now
+type KeyMode int
+const (
+	Major KeyMode = iota
+	Minor
+)
+
+type WorkTitle struct {
+	Kind string
+	Number int
+	Nickname *string // optional
+}
+
+type Key struct {
+	CentralNote Note
+    Mode KeyMode
+}
+
+type Composer struct {
+    Name string
+	BirthDate int
+	DeathDate *int // optional, the composer may be alive now :shrug:
+}
+
+type TempoMarking struct {
+	Name string
+	Bpm *int // optional, since many markings don’t have a strict BPM
+}
+
+// e.g.: Op. 27, No. 2 (Chopin's Nocturne in D-flat Major)
+type Catalog struct {
+	Name string // Op, D, K..
+	Number int
+    InsideNum *int // optional, it may not have a number.
+}
+
+type Movement struct {
+	Kind *string // optional, e.g. Scherzo, Finale, Alla breve
+	TempoMarkings []TempoMarking
+	Nickname *string // optional
+	Order int
+}
+
+type CompositionYear struct {
+	StartYear int
+	EndYear *int // optional, if the work has been finished in the same year of start
+}
+
+// the length of the work will be an approximation of the sum of the lengths of its movements.
+// recordings stored separately.
+type Work struct {
+	Id int
+	Title WorkTitle
+	Key Key
+	Composer Composer
+	Catalog Catalog
+	Movements []Movement
+	Year CompositionYear
+}
