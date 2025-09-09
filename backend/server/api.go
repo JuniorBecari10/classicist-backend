b8b34b2f7273e4a6ee93dc5c4a03f900adac7833
Antônio Carlos <antonioocarlos@proton.me>
2025-09-09 18:05:49 -0300

    feat (back-end): add SQL queries

--- backend/server/api.go
+++ backend/server/api.go
@@ -10,8 +10,4 @@ func setupApi(app *fiber.App) {
 	api.Get("/", func (c *fiber.Ctx) error {
 		return c.SendString("Start")
 	})
-	
-	api.Get("/hello", func (c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
 }
---
+++ backend/server/query/composer_id.sql
@@ -0,0 +1,2 @@
+SELECT * FROM composers
+WHERE id = ?;
