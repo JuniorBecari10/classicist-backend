75bbee0c7c2ddad71ab4b695760d51a404e2961e
Antônio Carlos <antonioocarlos@proton.me>
2025-10-30 15:10:19 -0300

    feat (backend): add CORS support

--- backend/server/server.go
+++ backend/server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 const PORT = 8080
@@ -12,6 +13,8 @@ const PORT = 8080
 func SetupServer() {
 	log.Printf("Starting server on port %d..\n", PORT)
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Static("/public", "../public/files")
 	setupApi(app)
